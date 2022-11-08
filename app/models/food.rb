a72cfc633ff73f1952b39040d7faabc09e2b9db3
adebowale <ademibowaleadegboye@gmail.com>
2022-11-08 14:36:40 +0100

    add recipes-helper

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,4 +6,6 @@ class Food < ApplicationRecord
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
-end
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :user_id, presence: true
+  end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,24 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :serves, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :ingredients, presence: true
+  validates :method, presence: true
+  validates :description, presence: true
+  validates :user_id, presence: true
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%") 
+  end
+
+  def self.search_by_ingredients(search)
+    where("ingredients LIKE ?", "%#{search}%") 
+  end
+
+  def self.search_by_method(search)
+    where("method LIKE ?", "%#{search}%") 
+  end
 end
+
+
+
