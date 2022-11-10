01f0c04554147e453fa940e57b02e334eb00893d
adebowale <ademibowaleadegboye@gmail.com>
2022-11-10 10:56:32 +0100

    added chabges food.rb

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,7 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
 
-
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
