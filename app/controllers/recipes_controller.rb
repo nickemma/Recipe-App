7e7f50b64fc6c74dd206f89c32b76191a28cd7af
adebowale <ademibowaleadegboye@gmail.com>
2022-11-10 11:00:21 +0100

    add recipes controler

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,9 +3,7 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
+  
 
   def new
     @recipe = Recipe.new
