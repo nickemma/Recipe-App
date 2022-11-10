5819741fa59331345736663afdd1917902c4afea
adebowale <ademibowaleadegboye@gmail.com>
2022-11-10 11:01:10 +0100

    add changes on recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,9 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
   def new
     @recipe = Recipe.new
